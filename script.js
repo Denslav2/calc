5f64f5b9adc9f11821e72d16c9beee46ca097f57
Den_Slav <joomlakop@gmail.com>
2021-01-15 18:31:52 +0200

    add btn clear

--- script.js
+++ script.js
@@ -1,52 +1,53 @@
-function plus () {
-    let num_1;
-    num_1 = document.getElementById('num_1').value;
+function plus() {
+    let num_1 = document.getElementById('area_1').value;
     num_1 = parseInt(num_1);
-    
-    let num_2;
-    num_2 = document.getElementById('num_2').value;
+
+    let num_2 = document.getElementById('area_2').value;
     num_2 = parseInt(num_2);
 
-    let result = num_1 + num_2;
-    document.getElementById('res').innerHTML = result;
-
+    let resultPlus = num_1 + num_2;
+    document.getElementById('res').innerHTML = resultPlus;
 }
 
-function minus () {
-    let num_1;
-    num_1 = document.getElementById('num_1').value;
+function minus() {
+    let num_1 = document.getElementById('area_1').value;
     num_1 = parseInt(num_1);
-    
-    let num_2;
-    num_2 = document.getElementById('num_2').value;
+
+    let num_2 = document.getElementById('area_2').value;
     num_2 = parseInt(num_2);
 
-    let m_result = num_1 - num_2;
-    document.getElementById('res').innerHTML = m_result;
+    let resultMinus = num_1 - num_2;
+    document.getElementById('res').innerHTML = resultMinus;
 }
 
-function mult () {
-    let num_1;
-    num_1 = document.getElementById('num_1').value;
+function mult(){
+    let num_1 = document.getElementById('area_1').value;
     num_1 = parseInt(num_1);
-    
-    let num_2;
-    num_2 = document.getElementById('num_2').value;
+
+    let num_2 = document.getElementById('area_2').value;
+    num_2 = parseInt(num_2)
+
+    let resultMult = num_1 * num_2;
+    document.getElementById('res').innerHTML = resultMult;
+}
+
+function divis() {
+    let num_1 = document.getElementById('area_1').value;
+    num_1 = parseInt(num_1);
+
+    let num_2 = document.getElementById('area_2').value;
     num_2 = parseInt(num_2);
 
-    let multResult = num_1 * num_2;
-    document.getElementById('res').innerHTML = multResult;
-}
+    let resultDivis = num_1 / num_2;
+    document.getElementById('res').innerHTML = resultDivis;
+}    
 
-function divis () {
-    let num_1;
-    num_1 = document.getElementById('num_1').value;
-    num_1 = parseInt(num_1);
-    
-    let num_2;
-    num_2 = document.getElementById('num_2').value;
-    num_2 = parseInt(num_2);
-
-    let divisResult = num_1 / num_2;
-    document.getElementById('res').innerHTML = divisResult;
-}
+document.querySelectorAll(".clear").forEach(function (elem){
+    elem.onclick = function (e) {
+        let selector = this.dataset.clearSelector;
+        document.querySelectorAll(selector)
+        .forEach(function (item) {
+          item.value = "";
+        });
+      };
+    });
